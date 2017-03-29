41be42d64d11f62840557c683b10bd3bdd9321bf
alberto56 <albert@mediatribe.net>
2017-03-29 16:22:54 -0400

    Use correct docker command in test

--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@ docker pull node
 docker pull dcycle/css-lint
 docker build -t local-dcycle-css-lint-image .
 
-docker run -v $(pwd)/example:/app/code dcycle/js-lint --help
-docker run -v $(pwd)/example:/app/code dcycle/js-lint .
+docker run -v $(pwd)/example:/app/code dcycle/css-lint --help
+docker run -v $(pwd)/example:/app/code dcycle/css-lint .
 docker run -v $(pwd)/example:/app/code local-dcycle-css-lint-image --help
 docker run -v $(pwd)/example:/app/code local-dcycle-css-lint-image .
