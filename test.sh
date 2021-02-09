a43277b41049d5fbcbd67d98e5f8155afdd452d9
Albert Albala <albert@mediatribe.net>
2021-02-09 14:31:12 -0500

    Please squash this commit in the pull request

--- test.sh
+++ test.sh
@@ -1,9 +1,6 @@
 set -e
 docker pull node
-docker pull dcycle/css-lint
 docker build -t local-dcycle-css-lint-image .
 
-docker run -v $(pwd)/example:/app/code dcycle/css-lint --help
-docker run -v $(pwd)/example:/app/code dcycle/css-lint .
 docker run -v $(pwd)/example:/app/code local-dcycle-css-lint-image --help
 docker run -v $(pwd)/example:/app/code local-dcycle-css-lint-image .
