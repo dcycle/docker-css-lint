2164f09d26ee7ef970680162cf8bf0c9ac9fd162
Albert Albala <albert@mediatribe.net>
2021-02-12 08:20:40 -0500

    Add --rm flag to docker run directives (#4)

--- test.sh
+++ test.sh
@@ -2,5 +2,5 @@ set -e
 docker pull node
 docker build -t local-dcycle-css-lint-image .
 
-docker run -v $(pwd)/example:/app/code local-dcycle-css-lint-image --help
-docker run -v $(pwd)/example:/app/code local-dcycle-css-lint-image .
+docker run --rm -v $(pwd)/example:/app/code local-dcycle-css-lint-image --help
+docker run --rm -v $(pwd)/example:/app/code local-dcycle-css-lint-image .
